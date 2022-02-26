50182d0d485c2ff743d9d31fd8fc0ed3df526040
kim yongbin <kybinz@gmail.com>
2022-02-26 09:32:45 +0000

    sqlite/service: fix Time.Scan handle input of type time.Time
    It is confusing, but the Scan get string sometimes,
    time.Time otherwise.

--- service/sqlite/time.go
+++ service/sqlite/time.go
@@ -6,11 +6,16 @@ import (
 
 type Time time.Time
 
-func (t *Time) Scan(v interface{}) error {
-	vt, err := time.Parse("2006-01-02 15:04:05.9-07:00", v.(string))
-	if err != nil {
-		return err
+func (t *Time) Scan(i interface{}) error {
+	switch v := i.(type) {
+	case time.Time:
+		*t = Time(v)
+	case string:
+		vt, err := time.Parse("2006-01-02 15:04:05.9-07:00", v)
+		if err != nil {
+			return err
+		}
+		*t = Time(vt)
 	}
-	*t = Time(vt)
 	return nil
 }
--- service/sqlite/time_test.go
+++ service/sqlite/time_test.go
@@ -3,18 +3,20 @@ package sqlite
 import (
 	"log"
 	"testing"
+	"time"
 )
 
 func TestParseTimeScan(t *testing.T) {
 	// sqlite uses variable length of string for micro seconds.
 	// Time.Scan should convert these string into time without having error.
 	cases := []struct {
-		s string
+		s interface{}
 	}{
 		{s: "0000-01-01 00:00:00.123456789+00:00"},
 		{s: "0000-01-01 00:00:00.123456+00:00"},
 		{s: "0000-01-01 00:00:00.123+00:00"},
 		{s: "0000-01-01 00:00:00+00:00"},
+		{s: time.Now()},
 	}
 	for _, c := range cases {
 		t := Time{}
